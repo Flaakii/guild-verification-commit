4faa710613caf1d5b07dbe7384c35ac3bd7ea281
Flaakii <flaakii23@outlook.com>
2026-03-10 03:21:10 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T00:20:53.170Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-10T00:21:10.939Z
+Commit: Fix memory leak in event handlers
