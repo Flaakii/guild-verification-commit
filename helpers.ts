783ffea9437432cf4d26bf72d9dd60fc4dec722b
Flaakii <flaakii23@outlook.com>
2026-03-10 03:22:02 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T00:21:53.681Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-10T00:22:03.162Z
+Commit: Fix broken links in documentation
