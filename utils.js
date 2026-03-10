708bfada4d3d3be30ef0182a7961c96bdd97b9e9
Flaakii <flaakii23@outlook.com>
2026-03-10 03:20:48 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T00:20:42.027Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-10T00:20:48.984Z
+Commit: Fix broken links in documentation
