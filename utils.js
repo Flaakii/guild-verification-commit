44c9ec93417dd2811a6213a3a7c521b18e8352a6
Flaakii <flaakii23@outlook.com>
2026-03-10 03:20:56 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T00:20:52.024Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-10T00:20:56.353Z
+Commit: Resolve timezone issues in date handling
