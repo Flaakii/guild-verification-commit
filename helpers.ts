4d1a782488537e75c6390addf10ea91de4422eee
Flaakii <flaakii23@outlook.com>
2026-03-10 03:22:05 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T00:22:03.162Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-10T00:22:06.147Z
+Commit: Patch security vulnerability in authentication
