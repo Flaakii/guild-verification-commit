be871b70e9c140d5aaf889a29e5f70c028603087
Flaakii <flaakii23@outlook.com>
2026-03-10 03:22:07 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T00:22:06.147Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-10T00:22:07.364Z
+Commit: Add export functionality for reports
