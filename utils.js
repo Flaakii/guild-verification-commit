4cddf8250a362a74181237c71dd0ab7e0b7ed3e0
Flaakii <flaakii23@outlook.com>
2026-03-10 03:22:00 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T00:21:54.974Z
-Commit: Add dark mode support
+Timestamp: 2026-03-10T00:22:00.536Z
+Commit: Resolve null pointer exception in user service
