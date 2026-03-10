63ffe5a1b74d6a2677d94f37237136452464b51a
Flaakii <flaakii23@outlook.com>
2026-03-10 03:22:17 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T00:22:15.625Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-10T00:22:17.479Z
+Commit: Fix memory leak in event handlers
