9b9b8895d641d864c47c34208e6ce2d0254d0b32
Flaakii <flaakii23@outlook.com>
2026-03-10 03:22:34 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T00:22:17.479Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-10T00:22:34.781Z
+Commit: Fix broken links in documentation
